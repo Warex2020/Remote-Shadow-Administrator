90bb568d32d0ea4ae416b83a8e3ca5eaf98a8430
Warex <warex@junix.de>
2023-03-12 14:11:24 +0100

    first translation but very bugy

---
+++ Versions/findCyrillic.ps1
@@ -0,0 +1,8 @@
+﻿$code = Get-Content RSA_1.4.ps1 -Raw
+
+$lines = $code.Split("`n")
+for ($i=0; $i -lt $lines.Count; $i++) {
+    if ($lines[$i] -match "[\p{IsCyrillic}]") {
+        Write-Output "Line $($i+1): $($lines[$i] | Select-String "[\p{IsCyrillic}]+")"
+    }
+}
